bbe411fd763ada035eb164d6e911f8c9c1676044
Lê Chí Huy <lechihuy1062001@gmail.com>
2024-07-29 22:27:26 +0700

    init

---
+++ index.tsx
@@ -0,0 +1,9 @@
+import { GlobeIcon } from "./globe-icon";
+import { HomeIcon } from "./home-icon";
+import { LockClosedIcon } from "./lock-closed-icon";
+import { UserIcon } from "./user-icon";
+import { UserScreenIcon } from "./user-screen-icon";
+
+export interface IconProps extends React.SVGAttributes<HTMLOrSVGElement> {}
+
+export { UserIcon, LockClosedIcon, HomeIcon, GlobeIcon, UserScreenIcon };
